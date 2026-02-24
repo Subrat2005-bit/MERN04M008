65bcf7a85560165e4355cc4dd0328314e7919ce0
Subrat Mahanandia <subratmahanandia@Subrats-MacBook-Air.local>
2026-02-24 11:51:17 +0530

    Java Script

---
+++ Week4/Day2/jscript.js
@@ -0,0 +1,26 @@
+console.log("I'm the best")
+a=14
+console.log(a)
+
+A=12
+console.log(A)
+
+a2=23
+console.log(a2)
+
+// new =23
+// console.log(new) ( new is reserved key word)
+
+na_me ="Subrat"
+console.log(na_me)
+
+
+var num1=23
+console.log(num1)
+
+
+{
+    var num3=13
+    console.log(num3)
+}
+console.log("outside", num3)
