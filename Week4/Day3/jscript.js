a9c48a3ce930d6781691de4421366fededc586aa
Subrat Mahanandia <subratmahanandia@Subrats-MacBook-Air.local>
2026-02-25 11:59:58 +0530

    Data type done

---
+++ Week4/Day3/jscript.js
@@ -0,0 +1,59 @@
+console.log("Hello");
+
+//Way of declaration
+var v;
+v=40
+console.log(v)
+
+var a = "hello"
+console.log(a)
+
+//Let
+let b;
+b=27
+console.log(b)
+
+
+//constnt
+const n=67
+console.log(n)
+
+
+
+//data type
+//primitive Data type
+//Number
+let num=4
+console.log(typeof num)
+
+//string
+let str="hi"
+console.log(str)
+
+//boolean
+let ispass=true;
+console.log(ispass)
+
+//Undifined
+let value= undefined;
+console.log(value)
+
+//nul
+let nullValue= null;
+console.log(typeof null)
+
+
+//data type
+//primitive Data type
+ let obj={
+    name:"subrat",
+    age:60,
+    ispassed:true
+ }
+ console.log( obj["age"])
+
+
+ //array
+ let arr=[2,3,4,5,6, {name:"subrat"}]
+ console.log(arr[5].name
+ );
