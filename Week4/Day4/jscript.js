8d3e8df624b411234bae2a7bc54e99f7e6595e6c
Subrat Mahanandia <subratmahanandia@Subrats-MacBook-Air.local>
2026-02-26 12:02:34 +0530

    operate done

---
+++ Week4/Day4/jscript.js
@@ -0,0 +1,73 @@
+//Sigle line comment oparater
+/* This is  multiple 
+line comment. */
+
+//Arithmatic operater
+// +,-, *, /, %, **
+console.log(2+3);
+console.log(4-2);
+console.log(5/2);
+console.log(5*2);
+console.log(5%2);
+console.log(5**3);
+
+
+//Increment
+let a=4
+console.log(a++);
+console.log(a);
+console.log(++a);
+
+
+//decrement
+let b=4
+console.log(b--);
+console.log(b);
+console.log(--b)
+
+//
+let c=4;
+c=c+5;
+console.log(c);
+
+//==
+console.log(2==2); // true
+console.log(2==1); // false
+console.log("true"== "true");// true
+console.log(2=="2"); // true
+console.log(1== true);// true
+console.log(true+2 == "2");// false
+console.log("true"== "true");
+
+
+//===
+console.log(2 === 1);//false
+console.log("2"===2);//false
+console.log("test" === "test");//True
+console.log(0==0); //true
+console.log(undefined == undefined);// true
+console.log(null == null);//true
+console.log(undefined == null);//true
+console.log(undefined == 0);//false
+console.log(undefined === true);//false
+console.log({}=={});// false
+console.log([]==[]);//false
+
+
+//!=
+console.log(1 != 1); // false
+console.log(1 != "1"); // false
+
+//logical operater
+//AND(&&), OR(||), NOT(!)
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && false);// false
+
+// OR(||)
+
+console.log(true || false); // true
+console.log(true || true); // true
+console.log(false || false); //false
+console.log(false || true); // true
+console.log(0 || 1); // 1( As a true), 0( false value)
